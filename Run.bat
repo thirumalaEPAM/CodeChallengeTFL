d3bdb6ea3df2ea2b37652f23600a9538905b84a1
thirumalaEPAM <thirumala_rajolu@epam.com>
2022-08-19 15:55:48 +0530

    New Updates

---
+++ Run.bat
@@ -0,0 +1,3 @@
+C:\Users\Thirumala_Rajolu\.nuget\packages\nunit.consolerunner\3.15.2\tools
+
+nunit3-console.exe "C:\Thirumala\PersonalDocs\TFLCodeChallenge2022\TFLCodeChallenge2022\bin\Debug\net6.0\TFLCodeChallenge2022.dll"
